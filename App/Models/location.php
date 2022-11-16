0aaeca2de44f41661402be5d4f56a15f20027784
Albert <ajuan@cendrassos.net>
2022-11-16 18:45:45 +0100

    espacioooooo

---
+++ App/Models/location.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Models;
+
+class Location
+{
+
+    private $sql;
+
+    public function __construct($config)
+    {
+        $dsn = "mysql:dbname={$config['db']};host={$config['host']}";
+        $usuari = $config["user"];
+        $clau = $config["pass"];
+        
+        try {
+            $this->sql = new PDO($dsn, $usuari, $clau);
+        } catch (PDOException $e) {
+            die('Ha fallat la connexió: ' . $e->getMessage());
+        }
+    }
+
+    public function insertLocation($name,$longitude,$latitude){
+
+        $query ='insert into espacio (nombre_espacio, longitud_espacio, latitud_espacio) values(:name,:longitude,:latitude)';
+        $stm = $this->sql->prepare($query);
+        $stm->execute(['name' => $name , 'longitude' => $longitude , 'latitude' => $latitude]);
+
+        if ($stm->errorCode() !== '00000') {
+            $err = $stm->errorInfo();
+            $code = $stm->errorCode();
+            throw new Exception("Error.   {$err[0]} - {$err[1]}\n{$err[2]} $query");
+        }
+    }
+
+    public function getLocation($id){
+
+        $query ='select * from espacio where id_espacio = :id';
+        $stm = $this->sql->prepare($query);
+        $result = $stm->execute([':id' => $id]);
+
+        if ($stm->errorCode() !== '00000') {
+            $err = $stm->errorInfo();
+            $code = $stm->errorCode();
+            throw new Exception("Error.   {$err[0]} - {$err[1]}\n{$err[2]} $query");
+        }
+    }
+
+    public function updateLocation($column,$newValue,$id){
+
+        $query = 'UPDATE espacio SET ' . $column . ' = :newValue WHERE id_espacio=:id';
+        $stm = $this->sql->prepare($query);
+        $stm->execute([':id' => $id,':newValue' => $newValue]);
+        if ($stm->errorCode() !== '00000') {
+            $err = $stm->errorInfo();
+            $code = $stm->errorCode();
+            throw new Exception("Error.   {$err[0]} - {$err[1]}\n{$err[2]} $query");
+        }
+    }
+
+    public function deleteLocation($id){
+
+        $query = 'delte from espacio where id_espacio = :id';
+        $stm = $this->sql->prepare($query);
+        $stm->execute([':id' => $id]);
+
+        if ($stm->errorCode() !== '00000') {
+            $err = $stm->errorInfo();
+            $code = $stm->errorCode();
+            throw new Exception("Error.   {$err[0]} - {$err[1]}\n{$err[2]} $query");
+        }
+
+    }
+
+}
