2a33b4201fbbab158567da20ece1cef269552293
Albert <ajuan@cendrassos.net>
2022-11-16 16:05:06 +0100

    vote done + minor fixes

---
+++ App/Models/vote.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Models;
+
+class Vote
+{
+
+    private $sql;
+
+    public function __construct($config)
+    {
+        $dsn = "mysql:dbname={$config['db']};host={$config['host']}";
+        $usuari = $config["user"];
+        $clau = $config["pass"];
+        
+        try {
+            $this->sql = new PDO($dsn, $usuari, $clau);
+        } catch (PDOException $e) {
+            die('Ha fallat la connexió: ' . $e->getMessage());
+        }
+    }
+
+    public function insertVote($id,$valoration){
+        
+        $query = 'insert into voto (id_espectaculo_voto,valoracion_voto) values (:id,:valoration)';
+        $stm = $this->sql->prepare($query);
+        $stm->execute([':id' => $id, ':valoration' => $valoration]);
+
+        if ($stm->errorCode() !== '00000') {
+            $err = $stm->errorInfo();
+            $code = $stm->errorCode();
+            throw new Exception("Error.   {$err[0]} - {$err[1]}\n{$err[2]} $query");
+        }
+
+    }
+
+    public function getAvgVote($id){
+
+        $query = 'select avg(valoracion_voto) from voto where id_espectaculo_voto=:id';
+        $stm = $this->sql->prepare($query);
+        $stm->execute([':id' => $id]);
+
+        if ($stm->errorCode() !== '00000') {
+            $err = $stm->errorInfo();
+            $code = $stm->errorCode();
+            throw new Exception("Error.   {$err[0]} - {$err[1]}\n{$err[2]} $query");
+        }
+
+    }
+
+
+}
