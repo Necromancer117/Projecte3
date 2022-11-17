4e4340355f177710946baa5a7acd0cbc097ccfa3
Aquilexus <paulsoual@gmail.com>
2022-11-17 13:52:31 +0100

    view login done

--- App/Views/portada.php
+++ App/Views/portada.php
@@ -12,26 +12,24 @@
   <script src="https://cdn.tailwindcss.com"></script>
 
 
-  <title>Exemple de portada</title>
+  <title>Fake circ</title>
 </head>
 
 <body>
   <div class="bg-cover bg-center bg-no-repeat" style="background-image: url(img/portada-bg.jpg)">
     <div class="flex flex-row underline decoration-white text-white font-bold bg-black bg-opacity-20">
       <div class=" text-[50px] my-4 ml-6">
-        <img class="w-[200px]" src="img/logo.png" alt="" srcset="">
+        <a href="#"><img class="w-[200px]" src="img/logo.png" alt="" srcset=""></a>
       </div>
-      <div class="flex flex-row gap-12 text-end justify-self-end ml-auto mr-20 mt-10 text-2xl">
+      <div class="flex flex-row gap-12 text-end justify-self-end ml-auto mr-20 mt-10 text-2xl text-black">
         <div>
-          <div>
-            <a class="rounded p-2 transition bg-white bg-opacity-0 duration-350 ease-in hover:bg-opacity-80 hover:text-black" href="#">FakeFest 2023</a>
-          </div>
+          <a class="rounded p-2 transition bg-white duration-200 ease-in hover:bg-black hover:text-white" href="#">FakeCirc 2023</a>
         </div>
         <div>
-          <a class="rounded p-2 transition bg-white bg-opacity-0 duration-350 ease-in hover:bg-opacity-80 hover:text-black" href="#">Old Fest</a>
+          <a class="rounded p-2 transition bg-white duration-200 ease-in hover:bg-black hover:text-white" href="#">Old Circ</a>
         </div>
         <div class="ml-6">
-          <a class="rounded p-2 transition bg-white bg-opacity-0 duration-350 ease-in hover:bg-opacity-80 hover:text-black" href="#">Sign In</a>
+          <a class="rounded p-2 transition bg-white duration-200 ease-in hover:bg-black hover:text-white" href="/login">Sign In</a>
         </div>
       </div>
       <div class="text-[30px] flex flex-row gap-3 mr-10 mt-7">
@@ -46,7 +44,7 @@
         </div>
       </div>
     </div>
-    <div class="h-[80vh] flex">
+    <div class="h-[90vh] flex">
       <div class="xl:w-[500px] md:w-[450px]  rounded text-white ml-10 h-[90%] font-bold text-center my-auto bg-black bg-gradient-to-r p-[6px] from-[#BF953F] via-[#FCF6BA] to-[#B38728]">
         <div class="flex flex-col justify-between h-full bg-black text-white rounded-lg p-4">
           <span class="font-extrabold my-auto text-transparent lg:text-7xl md:text-6xl bg-clip-text bg-gradient-to-r from-[#BF953F] via-[#FCF6BA] to-[#B38728]">THE GREATEST SHOW OF EUROPE!</span>
@@ -54,6 +52,7 @@
       </div>
     </div>
     <div class="bg-white">
+
     </div>
   </div>
 
