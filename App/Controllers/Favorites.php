125312cb77bbb9863eb61d19c8b5561a86952097
Aquilexus <paulsoual@gmail.com>
2022-11-30 20:34:24 +0100

    favorites working + animation

--- App/Controllers/Favorites.php
+++ App/Controllers/Favorites.php
@@ -2,21 +2,40 @@
 
 namespace App\Controllers;
 
-class Favorites{
+class Favorites
+{
 
     public $container;
 
     public function __construct($container)
     {
-        $this->container=$container;
+        $this->container = $container;
     }
 
 
-    public function addFavorite($request,$response,$container){
+    public function addFavorite($request, $response, $container)
+    {
 
-        $user_id = $request->get('SESSION','id');
-        $id_show = $request->get(INPUT_POST,'value');
-        echo($id_show);
+        $user_id = $request->get('SESSION', 'id');
+        $id_show = $request->get(INPUT_POST, 'value');
+        $selector = $request->get(INPUT_POST, 'selector');
+
+        switch ($selector) {
+            case 'add':
+                $favorite = $container->get('favorite');
+                $favorite->insertFavorite($user_id, $id_show);
+                break;
+
+            case 'remove':
+                $favorite = $container->get('favorite');
+                $favorite->deleteFavorite($user_id, $id_show);
+                break;
+
+            default:
+                # code...
+                break;
+        }
+
         return $response;
     }
 }
