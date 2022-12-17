cc7341918a6bebde1c8a5db502f81ad3f1d1a5c0
Marc Vidal <marcvidalardevol@gmail.com>
2022-12-17 11:15:30 +0100

    fa falta que pugui llegir el js

--- App/Views/creator.php
+++ App/Views/creator.php
@@ -11,8 +11,10 @@
     <!-- Tailwind CSS -->
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="/styles.css">
+    <script src="js/bundle.js"></script>
 
     <title></title>
+
 </head>
 
 <nav class="bg-gray-800">
@@ -66,13 +68,13 @@
                 <!-- Profile dropdown -->
                 <div class="relative ml-3">
                     <div>
-                        <button type="button" class="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+                        <button type="button" id="profileImage" class="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
                             <span class="sr-only">Open user menu</span>
-                            <img id="avatar" name='avatar' class="h-8 w-8 rounded-full" src="/img/<?php echo ($avatar) ?>" alt="" srcset="">
+                            <img name='avatar' class="h-8 w-8 rounded-full" src="/img/<?php echo ($avatar) ?>" alt="" srcset="">
                         </button>
                     </div>
 
-                    <div id="creatorDropDown" class="hidden absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
+                    <div id="creatorDropDown" hidden class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
                         <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
                         <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem" tabindex="-1" id="user-menu-item-1">Support</a>
                         <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</a>
@@ -161,7 +163,6 @@
 
         </div>
     </div>
-    <script src="/js/bundle.js"></script>
 </body>
 
 </html>
--- App/js/creatorNavbar.js
+++ App/js/creatorNavbar.js
@@ -1,30 +1,16 @@
 import $ from "jquery";
 
-var show = false
-export default function creatorNavBar() {
+var show = false;
 
-    $(function() {
-        $('#avatar').on('click',function () {
-            if (!show) {
-                show=!show;
-                $('#creatorDropDown').removeClass('hidden');
-                $('.user_options').removeClass('hidden');
-                $(".user_options").animate({
-                    opacity: '1',
-                    height: '170px',
-                    width: '150px'
-                });
-            }
-            else{
-                $('.item').addClass('hidden');
-                show=!show;
-                $(".user_options").animate({
-                    opacity: '0',
-                    height: '0px',
-                    width: '0px'
-                });
-                //$('#user_options').addClass('invisible');
-            }
-        });
-    });
+export default function cratorNavBar() {
+    $('#creatorDropDown').show();
+
+    $('#profileImage').on('click', function () {
+        show = !show;
+        if (show) {
+            $('#creatorDropDown').show();
+        } else {
+            $('#creatorDropDown').hide();
+        }
+    })
 }
