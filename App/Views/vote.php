c936f88d9796c3e3063318aa9990ee3abccc03e2
Aquilexus <paulsoual@gmail.com>
2022-12-20 22:47:46 +0100

    page detects if user have already voted

--- App/Views/vote.php
+++ App/Views/vote.php
@@ -30,44 +30,54 @@
             <img src="/img/shows/<?php echo ($show['imagen_espectaculo']) ?>" alt="">
         </div>
         <!-- RATING -->
-        <div id="setRate" class="border-lg flex gap-1 flex-col items-center mt-6">
-            <div class="bg-black text-white bg-opacity-60 border rounded-lg border-black px-28 py-4">
-                <div class="text-center flex flex-col gap-3">
-                    <p id="emoji" class="text-4xl">&#10024</p>
-                    <p id="rating" class="text-3xl font-thin">Rating</p>
+        <?php if (!$voted) { ?>
+
+
+            <div id="setRate" class="border-lg flex gap-1 flex-col items-center mt-6">
+                <div class="bg-black text-white bg-opacity-60 border rounded-lg border-black px-28 py-4">
+                    <div class="text-center flex flex-col gap-3">
+                        <p id="emoji" class="text-4xl">&#10024</p>
+                        <p id="rating" class="text-3xl font-thin">Rating</p>
+                    </div>
+                    <div class="flex text-4xl text-yellow-400">
+                        <div class="star" id="star-1" data-rate="1">
+                            <i class=" bi bi-star"></i>
+                            <p class="text-white text-xl text-center">1</p>
+                        </div>
+                        <div class="star" id="star-2" data-rate="2">
+                            <i class=" bi bi-star"></i>
+                            <p class="text-white text-xl text-center">2</p>
+                        </div>
+                        <div class="star" id="star-3" data-rate="3">
+                            <i class=" bi bi-star"></i>
+                            <p class="text-white text-xl text-center">3</p>
+                        </div>
+                        <div class="star" id="star-4" data-rate="4">
+                            <i class=" bi bi-star"></i>
+                            <p class="text-white text-xl text-center">4</p>
+                        </div>
+                        <div class="star" id="star-5" data-rate="5">
+                            <i class=" bi bi-star"></i>
+                            <p class="text-white text-xl text-center">5</p>
+                        </div>
+                    </div>
                 </div>
-                <div class="flex text-4xl text-yellow-400">
-                    <div class="star" id="star-1" data-rate="1">
-                        <i class=" bi bi-star"></i>
-                        <p class="text-white text-xl text-center">1</p>
-                    </div>
-                    <div class="star" id="star-2" data-rate="2">
-                        <i class=" bi bi-star"></i>
-                        <p class="text-white text-xl text-center">2</p>
-                    </div>
-                    <div class="star" id="star-3" data-rate="3">
-                        <i class=" bi bi-star"></i>
-                        <p class="text-white text-xl text-center">3</p>
-                    </div>
-                    <div class="star" id="star-4" data-rate="4">
-                        <i class=" bi bi-star"></i>
-                        <p class="text-white text-xl text-center">4</p>
-                    </div>
-                    <div class="star" id="star-5" data-rate="5">
-                        <i class=" bi bi-star"></i>
-                        <p class="text-white text-xl text-center">5</p>
-                    </div>
+            <?php } else { ?>
+                <div class="text-4xl mx-auto lg:w-1/2 mt-3 mb-3 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded">
+                    You have already voted !
                 </div>
+            <?php } ?>
             </div>
-        </div>
-        <!-- END RATING -->
-        <div id="thankYou" class="mx-auto opacity-0 hidden">
-            <img class="w-[300px]" src="/img/thank-you-6.gif" alt="">
-        </div>
-        <div id="vote_send" class="opacity-0 text-center my-5">
-            <button class="bg-red-600 text-white text-lg py-2 px-6 border rounded-lg" data-show_id="<?php echo ($show['id_espectaculo']) ?>">Send</button>
-        </div>
+
+            <div id="thankYou" class="mx-auto opacity-0 hidden">
+                <img class="w-[300px]" src="/img/thank-you-6.gif" alt="">
+            </div>
+            <div id="vote_send" class="opacity-0 text-center my-5">
+                <button class="bg-red-600 text-white text-lg py-2 px-6 border rounded-lg" data-show_id="<?php echo ($show['id_espectaculo']) ?>">Send</button>
+            </div>
     </div>
+
+    <!-- END RATING -->
     <!-- END MAIN CONTENT -->
     <?php include '../App/Views/footer.php' ?>
     <script src="js/bundle.js"></script>
