2b4597c2eadc3e7d3184196ac06434f85a2519ea
Marc Vidal <marcvidalardevol@gmail.com>
2022-12-20 09:52:03 +0100

    edit semi acabat

--- App/js/index.js
+++ App/js/index.js
@@ -2,17 +2,16 @@ import $ from "jquery";
 import login from "./login.js";
 import navbar from "./navbar.js"
 import map from './map.js'
-import account from "./accountSettings";
+import account from "./accountSettings.js";
 import favorites from "./favorites.js";
 import adminpanel from './adminpanel.js'
 import rate from "./rate.js";
-import portada_form from "./portada_form";
-import search from "./search_table";
-import search_table from "./search_table";
+import portada_form from "./portada_form.js";
+import search from "./search_table.js";
+import search_table from "./search_table.js";
 import graph from "./graph.js"
-import creatorNavbar from "./creatorNavbar.js"
-import AlpineFocusModal from "alpinejs-focus-modal";
-
+import creatorNavBar from "./creatorNavbar.js"
+import creatorShowsDropdwon from "./creatorShows.js"
 
 $(document).ready(function () {
     login();
